653c2f83ed7d63a9a2d036ea30999b8f4c9df232
Harish Nemade <162778561+harishnemade100@users.noreply.github.com>
2025-09-14 21:54:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// script.js
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+const themeToggle = document.getElementById('theme-toggle');
 
-document.getElementById('theme-toggle').addEventListener('click', function() {
+menuToggle.addEventListener('click', () => {
+  navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
+});
+
+themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
